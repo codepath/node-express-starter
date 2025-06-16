18166e7fc83f6f0b23c217b9a140aa8b88b6e5f3
Greg Merrill <gmerrill03@gmail.com>
2025-06-16 12:08:58 -0700

    more demo setup

--- api/server.js
+++ api/server.js
@@ -1,25 +1,33 @@
 // express server
 const express = require("express")
+const Pet = require("./pet-model")
 
 const server = express()
 
-server.get('/api/pets', (req, res, next) => {
-    res.status(200).json([
-        {
-            id: 1,
-            name: 'Fido',
-        },
-        {
-            id: 2,
-            name: 'Bicho',
-        },
-    ])
+server.get("/api/pets", async (req, res, next) => {
+	try {
+		const pets = await Pet.find(req.query)
+		if (pets.length) {
+			res.json(pets)
+		} else {
+			res.status(404).json({ message: "No pets found." })
+		}
+	} catch (err) {
+		next(err)
+	}
 })
 
-server.get('/favicon.ico', (req, res) => res.status(204).end())
+server.get("/favicon.ico", (req, res) => res.status(204).end())
 
 server.use("/{*splat}", (req, res, next) => {
-	res.status(200).json("You called the server")
+	res.status(404).json("Not found!")
+	next()
+})
+
+server.use((err, req, res, next) => {
+	const { message, status = 500 } = err
+	console.log(`${status}: ${message}`)
+	res.status(status).json({ message }) // <- does this do anything??
 })
 
 module.exports = server
