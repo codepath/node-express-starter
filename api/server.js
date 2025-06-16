e9772a348e91af23e0c7478896318dbf41664e2e
Greg Merrill <gmerrill03@gmail.com>
2025-06-16 11:02:05 -0700

    get basic express server running

--- api/server.js
+++ api/server.js
@@ -1,1 +1,25 @@
 // express server
+const express = require("express")
+
+const server = express()
+
+server.get('/api/pets', (req, res, next) => {
+    res.status(200).json([
+        {
+            id: 1,
+            name: 'Fido',
+        },
+        {
+            id: 2,
+            name: 'Bicho',
+        },
+    ])
+})
+
+server.get('/favicon.ico', (req, res) => res.status(204).end())
+
+server.use("/{*splat}", (req, res, next) => {
+	res.status(200).json("You called the server")
+})
+
+module.exports = server
--- index.js
+++ index.js
@@ -1,1 +1,9 @@
 // entry point of the program
+const server = require("./api/server")
+
+const PORT = process.env.PORT || 3000
+
+server.listen(PORT, () => {
+	console.log(`Server listening on ${PORT}`)
+})
+
